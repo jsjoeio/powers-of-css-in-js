37cbbf5492aa84340008de118a9c3c907a816334
Joe Previte <jjprevite@gmail.com>
2019-03-08 07:55:13 -0700

    Fix date in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -17,6 +17,6 @@ const Container = styled.footer`
 export default () => (
   <Container>
     <Link href='https://www.byteconf.com/js-2019' target='_blank' alt="Byteconf JavaScript 2019."><p style={{ margin: '10px 0' }}>Byteconf JavaScript 2019</p></Link>
-    <p style={{ margin: '0' }}>Saturday, March 23rd, 2019</p>
+    <p style={{ margin: '0' }}>Friday, March 22nd, 2019</p>
   </Container>
 )
